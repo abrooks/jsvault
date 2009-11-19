64999100624040c0a6656144acdedbe995523309
Chouser <chouser@n01se.net>
2009-11-19 13:02:12 -0500

    Add aes and json libs and sample db files

---
+++ sample-igv-input.js
@@ -0,0 +1,16 @@
+jsdb={
+ tags: ["shared with family", "personal", "sentry", "4d"],
+ accounts:
+  [{title: "Google",
+    url: "http://google.com/",
+	username: "chouser",
+	password: "likeIwouldTellYOU!",
+    comments: "I don't remember what I use this account for",
+    tags: ["personal"],
+    created: "date created",
+    modified: "date last modified"},
+   {title: "Ebay",
+	url: "http://ebay.com/",
+	username: "chouser",
+	password: "thisisforebay",
+	tags: ["personal", "sentry", "4d"]}]};
